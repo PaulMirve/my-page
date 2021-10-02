6a217c9f7166f4ecd02ba9898da65e2d6fedcf73
Paul Miranda Vega <paul.miranda@sparktechs.com>
2021-10-02 14:11:00 -0700

    first commit

---
+++ src/layout/Landing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const Landing: React.FC<Props> = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default Landing
